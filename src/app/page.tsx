64a9c15275d3c20f05977ec69e6bb04afb43be5d
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-10-02 20:38:09 +0900

    style: Home css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,46 +34,54 @@ export default async function Home() {
   const randomImageUrl3 = getRandomImageUrl(champions);
 
   return (
-    <div className="flex flex-col gap-5 justify-center items-center text-center">
-      <h1>리그 오브 레전드 정보 앱</h1>
-      <p>Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</p>
+    <div className="flex flex-col gap-8 justify-center items-center text-center bg-gray-950 min-h-screen py-10">
+      <h1 className="main-title">리그 오브 레전드 정보 앱</h1>
+      <p className="text-md text-gray-500">
+        Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.
+      </p>
 
-      <>
+      <div className="flex flex-col gap-6 items-center">
         <Link href={"/champions"}>
-          <div>
+          <div className="card-base">
             <Image
               src={randomImageUrl1}
               alt="Random Champion"
               width={500}
               height={500}
             />
-            <h2>챔피언 목록 보기</h2>
+            <h2 className="text-lg font-semibold text-purple-500 mt-3">
+              챔피언 목록 보기
+            </h2>
           </div>
         </Link>
 
         <Link href={"/items"}>
-          <div>
+          <div className="card-base">
             <Image
               src={randomImageUrl2}
               alt="Random Champion 3"
               width={500}
               height={500}
             />
-            <h2>아이템 목록 보기</h2>
+            <h2 className="text-lg font-semibold text-purple-500 mt-3">
+              아이템 목록 보기
+            </h2>
           </div>
         </Link>
         <Link href={"/rotation"}>
-          <div>
+          <div className="card-base">
             <Image
               src={randomImageUrl3}
               alt="Random Champion 3"
               width={500}
               height={500}
             />
-            <h2>금주 로테이션 확인</h2>
+            <h2 className="text-lg font-semibold text-purple-500 mt-3">
+              금주 로테이션 확인
+            </h2>
           </div>
         </Link>
-      </>
+      </div>
     </div>
   );
 }
