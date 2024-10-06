720f8ce9c15806977cce4b145ce697b4bf638b63
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-10-07 07:46:40 +0900

    update: react query 작업중

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,15 @@ import "./globals.css";
 import "../styles/reset.css";
 import Link from "next/link";
 import localFont from "next/font/local";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Koddi = localFont({
   src: "../public/fonts/KoddiUDOnGothic-Regular.woff2",
   display: "swap",
   variable: "--font-Koddi",
 });
+
+const queryClient = new QueryClient();
 
 export const metadata: Metadata = {
   title: "Riot Dex",
@@ -23,45 +26,50 @@ export default function RootLayout({
   return (
     <html lang="ko" className={Koddi.className}>
       <body>
-        <header className="bg-gray-900 text-white p-6 fixed top-0 w-full h-16 z-10">
-          <nav className="flex justify-end gap-4">
-            <Link href={"/"} className="hover:text-purple-500 transition-all ">
-              홈
-            </Link>
+        <QueryClientProvider client={queryClient}>
+          <header className="bg-gray-900 text-white p-6 fixed top-0 w-full h-16 z-10">
+            <nav className="flex justify-end gap-4">
+              <Link
+                href={"/"}
+                className="hover:text-purple-500 transition-all "
+              >
+                홈
+              </Link>
 
-            <Link
-              href={"/champions"}
-              className="hover:text-purple-500 transition-all"
-            >
-              챔피언 목록
-            </Link>
+              <Link
+                href={"/champions"}
+                className="hover:text-purple-500 transition-all"
+              >
+                챔피언 목록
+              </Link>
 
-            <Link
-              href={"/items"}
-              className="hover:text-purple-500 transition-all"
-            >
-              아이템 목록
-            </Link>
+              <Link
+                href={"/items"}
+                className="hover:text-purple-500 transition-all"
+              >
+                아이템 목록
+              </Link>
 
-            <Link
-              href={"/rotation"}
-              className="hover:text-purple-500 transition-all"
-            >
-              챔피언 로테이션
-            </Link>
-          </nav>
-        </header>
+              <Link
+                href={"/rotation"}
+                className="hover:text-purple-500 transition-all"
+              >
+                챔피언 로테이션
+              </Link>
+            </nav>
+          </header>
 
-        <main className="py-6 pt-20 pb-20">{children}</main>
-        <footer className="bg-gray-900 p-4 mt-8 fixed bottom-0 w-full h-16">
-          <p className="text-center text-white text-sm">
-            [riot-dex] is not endorsed by Riot Games and does not reflect the
-            views or opinions of Riot Games or anyone officially involved in
-            producing or managing Riot Games properties. Riot Games and all
-            associated properties are trademarks or registered trademarks of
-            Riot Games, Inc.
-          </p>
-        </footer>
+          <main className="py-6 pt-20 pb-20">{children}</main>
+          <footer className="bg-gray-900 p-4 mt-8 fixed bottom-0 w-full h-16">
+            <p className="text-center text-white text-sm">
+              [riot-dex] is not endorsed by Riot Games and does not reflect the
+              views or opinions of Riot Games or anyone officially involved in
+              producing or managing Riot Games properties. Riot Games and all
+              associated properties are trademarks or registered trademarks of
+              Riot Games, Inc.
+            </p>
+          </footer>
+        </QueryClientProvider>
       </body>
     </html>
   );
--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,72 +1,69 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { getChampionRotation } from "@/utils/riotApi";
-import { fetchChampionList } from "@/utils/serverApi"; // 챔피언 목록
+import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import Image from "next/image";
+import { getChampionRotation } from "@/utils/riotApi";
+import { fetchChampionList } from "@/utils/serverApi";
+import { ChampionData, Champion } from "@/types/Champion"; // ChampionData 타입을 가져옴
 
-type ChampionData = {
-  id: string;
-  key: string;
-  title: string;
-  name: string;
-  image: { full: string };
+const useChampionList = () => {
+  return useQuery({
+    queryKey: ["championList"],
+    queryFn: fetchChampionList,
+    staleTime: 1000 * 60 * 5, // 5분 동안 캐시
+  });
 };
 
-export default function ChampionRotationPage() {
-  const [rotation, setRotation] = useState<number[]>([]); // id , 배열로 담기
-  const [champions, setChampions] = useState<{
-    [key: string]: ChampionData;
-  } | null>(null);
-  // 챔피언 목록
+const useChampionRotation = () => {
+  return useQuery({
+    queryKey: ["championRotation"],
+    queryFn: getChampionRotation,
+    enabled: false, // refetch를 통해 데이터를 가져옵니다.
+  });
+};
 
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+const Rotation = () => {
+  const {
+    data: championData,
+    isLoading: isChampionLoading,
+    isError: isChampionError,
+  } = useChampionList();
+
+  const {
+    data: rotateData,
+    isLoading: isRotateLoading,
+    isError: isRotateError,
+    refetch: fetchRotation,
+  } = useChampionRotation();
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const rotationData = await getChampionRotation();
-        setRotation(rotationData.freeChampionIds);
-
-        const championData = await fetchChampionList();
-        setChampions(championData);
-      } catch (error) {
-        console.error("Fetch Error:", error);
-        setError("로테이션 데이터를 가져오는 중 문제가 발생했습니다.");
-      } finally {
-        setLoading(false);
-      }
+    if (championData) {
+      fetchRotation();
     }
+  }, [championData, fetchRotation]);
 
-    fetchData();
-  }, []);
+  if (isChampionLoading || isRotateLoading) return <div>로딩중...</div>;
+  if (isChampionError || isRotateError) return <div>에러가 발생했습니다.</div>;
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (!rotation || !champions) {
-    return <div>로테이션 또는 챔피언 데이터를 불러오는 중입니다...</div>;
-  }
+  // championData와 rotation 데이터가 존재할 때만 처리
+  const champions: ChampionData | undefined = championData?.data;
+  const rotation: number[] | undefined = rotateData?.freeChampionIds;
 
   return (
-    <div className="flex flex-col justify-center items-center gap-6  bg-gray-950 min-h-screen">
+    <div className="flex flex-col justify-center items-center gap-6 bg-gray-950 min-h-screen">
       <h1 className="main-title">챔피언 로테이션</h1>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5 mt-4 ">
-        {rotation.map((id) => {
-          const champion = Object.values(champions).find((champion) => {
-            return champion.key === id.toString();
-          });
+        {rotation?.map((id: number) => {
+          // champion을 Object.values에서 찾기
+          const champion = champions
+            ? Object.values(champions).find(
+                (champion) => champion.key === id.toString()
+              )
+            : undefined;
 
-          if (!champion) {
-            return null;
-          }
+          if (!champion) return null;
 
           return (
             <Link
@@ -74,7 +71,7 @@ export default function ChampionRotationPage() {
               href={`/champions/${champion.id}`}
               className="text-center"
             >
-              <div className=" rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300 ease-in-out p-4 text-center">
+              <div className="rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300 ease-in-out p-4 text-center">
                 <Image
                   src={`https://ddragon.leagueoflegends.com/cdn/14.19.1/img/champion/${champion.id}.png`}
                   alt={champion.name}
@@ -82,7 +79,6 @@ export default function ChampionRotationPage() {
                   height={100}
                   className="object-cover mx-auto hover:shadow-purple-100/20 hover:shadow-2xl"
                 />
-
                 <h3 className="text-md font-semibold text-purple-500 truncate mt-5">
                   {champion.name}
                 </h3>
@@ -96,4 +92,6 @@ export default function ChampionRotationPage() {
       </div>
     </div>
   );
-}
+};
+
+export default Rotation;
